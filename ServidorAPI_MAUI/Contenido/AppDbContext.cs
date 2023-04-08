549970b80265938834ca6c28d64d600fe4dcf2eb
ANDYVENUS <ANDYVENUS@ANDYVENUS>
2023-04-08 12:56:00 -0400

    Agregar archivos de proyecto.

---
+++ ServidorAPI_MAUI/Contenido/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServidorAPI_MAUI.Models;
+
+namespace ServidorAPI_MAUI.Contenido
+{
+    public class AppDbContext: DbContext
+
+    {
+        public DbSet<Plato> Platos => Set<Plato>();
+        public AppDbContext(DbContextOptions<AppDbContext> op) : base(op)
+        {  
+
+        }
+    }
+}
